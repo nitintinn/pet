96070a1a14fb65829f04eca6881b5736a33a8504
nitintinn <nitinlakra987@gmail.com>
2023-09-17 12:48:11 +0530

    wrapping elevated button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pet/features/auth/screens/auth_screen.dart';
 import 'package:pet/router.dart';
+import 'constants/global_variables.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,14 +16,12 @@ class MyApp extends StatelessWidget {
       title: 'Pet Transit',
       theme: ThemeData(
           primarySwatch: Colors.amber,
-          scaffoldBackgroundColor: Colors.white,
           appBarTheme: const AppBarTheme(
               elevation: 0, iconTheme: IconThemeData(color: Colors.black))),
       onGenerateRoute: (settings) => generateRoute(settings),
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
           title: const Text(
             'Pet Transit',
             style: TextStyle(color: Colors.black),
@@ -33,11 +32,13 @@ class MyApp extends StatelessWidget {
             const Center(
               child: Text('Flutter Demo Home Page'),
             ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, AuthScreen.routeName);
-                },
-                child: Text(" Continue  "))
+            Builder(builder: (context) {
+              return ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, AuthScreen.routeName);
+                  },
+                  child: const Text(" Continue  "));
+            })
           ],
         ),
       ),
